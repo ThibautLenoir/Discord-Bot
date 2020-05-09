00c73293ee78249773d6e78042b2da06465f1102
Thibaut Lenoir <thibaut.lenoir@student.howest.be>
2020-05-09 14:54:29 +0200

    Add folder for commands + test ping command

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ const {
     token
 } = require('./config.json');
 
-const ytdl = require('ytdl-core');
-
 const client = new Client();
-
-client.login(token);
 
 client.on('ready', () => {
     console.log('Running...');
@@ -22,6 +18,10 @@ client.on('ready', () => {
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(' ');
+    const args = message.content.slice(prefix.length).split('/ +/');
     const command = args.shift().toLowerCase();
+
+    console.log(command);
 });
+
+client.login(token);
