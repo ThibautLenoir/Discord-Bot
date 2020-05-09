b42769ffc04ff60c5ed1b8a3076737d027da37fe
Thibaut Lenoir <thibaut.lenoir@student.howest.be>
2020-05-09 15:02:23 +0200

    Check command collection for command and execute if exists

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 'use strict';
 
 const {
-    Client
-} = require('discord.js');
-
-const {
     prefix,
     token
 } = require('./config.json');
 
 const fs = require('fs');
+const Discord = require('discord.js');
 
-const client = new Client();
+const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -31,7 +28,14 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).split('/ +/');
     const command = args.shift().toLowerCase();
 
-    console.log(command);
+    if (!client.commands.has(command)) return;
+
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
 });
 
 client.login(token);
