ad4e69615ed12e963c2d926c0c2a534dff310cff
Thibaut Lenoir <thibaut.lenoir@student.howest.be>
2020-05-09 20:53:07 +0200

    Add jojo option for youre approaching me meme

--- commands/jojo.js
+++ commands/jojo.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports = {
     name: 'jojo',
     description: 'Jojo commands',
@@ -11,5 +13,20 @@ module.exports = {
 };
 
 const test = message => {
-    message.channel.send('Oh');
+    const dioEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setDescription('Oh? You\'re approaching me? Instead of running away, you\'re coming right to me? Even though your grandfather, Joseph, told you the secret of za warudo like an exam student scrambling to finish the last problems on an exam until the last moments before the chime?')
+        .setImage('https://i.imgur.com/tXIEjpm.jpg');
+
+    const jotaroEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setDescription('I can beat the shit out of you without getting closer.')
+        .setImage('https://i.imgur.com/hlsXDmM.png');
+
+    message.channel.send(dioEmbed);
+    message.channel.send(jotaroEmbed);
+
+    dioEmbed.setDescription('Oh ho! Then come as close as you like.');
+
+    message.channel.send(dioEmbed);
 };
