85b3aa899a4c6f669f3591d69a3c18260490dd92
Thibaut Lenoir <thibaut.lenoir@student.howest.be>
2020-05-09 14:43:58 +0200

    Remove test code

--- index.js
+++ index.js
@@ -11,46 +11,15 @@ const {
 } = require('./config.json');
 
 const ytdl = require('ytdl-core');
-const config = require('./config.json');
 
 const client = new Client();
-let mainVC;
 
-client.login(config.token);
+client.login(token);
 
 client.on('ready', () => {
-    mainVC = client.channels.cache.get("433272666414383116");
+    console.log('Running...');
 });
 
 client.on('message', message => {
-    checkForAlia(message);
-    checkforLeemhouse(message);
+
 });
-
-const checkForAlia = message => {
-    if (message.content === 'alia intro') {
-        if (!mainVC) {
-            console.log("Channel not found");
-        } else {
-            mainVC.join().then(connection => {
-                connection.play(ytdl('https://www.youtube.com/watch?v=HSumM_jNY3g', {
-                    filter: 'audioonly'
-                }));
-            });
-        }
-    }
-};
-
-const checkforLeemhouse = message => {
-    if (message.content === 'leemhouse rave') {
-        if (!mainVC) {
-            console.log("Channel not found");
-        } else {
-            mainVC.join().then(connection => {
-                connection.play(ytdl('https://www.youtube.com/watch?v=u_0A40FveIc', {
-                    filter: 'audioonly'
-                }));
-            });
-        }
-    }
-};
