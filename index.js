34aa70eeb3abde8ea30526ac19ca874d940112dd
Thibaut Lenoir <thibaut.lenoir@student.howest.be>
2020-05-09 15:04:48 +0200

    Refactor code

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+    const commandName = require(`./commands/${file}`);
+    client.commands.set(commandName.name, commandName);
 }
 
 client.on('ready', () => {
@@ -26,12 +26,14 @@ client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split('/ +/');
-    const command = args.shift().toLowerCase();
+    const commandName = args.shift().toLowerCase();
 
-    if (!client.commands.has(command)) return;
+    if (!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
 
     try {
-        client.commands.get(command).execute(message, args);
+        command.execute(message, args);
     } catch (error) {
         console.error(error);
         message.reply('there was an error trying to execute that command!');
