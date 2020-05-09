da5d097413357699bd6efb2c2b74d7eaaa6e7217
Thibaut Lenoir <thibaut.lenoir@student.howest.be>
2020-05-09 16:10:20 +0200

    Add ability to join channel and play song

--- commands/play.js
+++ commands/play.js
@@ -1,9 +1,19 @@
+const ytdl = require('ytdl-core');
+
 module.exports = {
     name: 'play',
     description: 'Play music using youtube links!',
-    args: true,
     usage: '<youtube url>',
+    args: true,
     execute(message, args) {
-        message.channel.send('Pong.');
+        if (message.member.voice.channel) {
+            message.member.voice.channel.join().then(connection => {
+                connection.play(ytdl(args[0], {
+                    filter: 'audioonly'
+                }));
+            })
+        } else {
+            console.log("Can't find channel");
+        }
     },
 };
--- index.js
+++ index.js
@@ -14,8 +14,8 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const commandName = require(`./commands/${file}`);
-    client.commands.set(commandName.name, commandName);
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
 }
 
 client.on('ready', () => {
@@ -25,7 +25,7 @@ client.on('ready', () => {
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split('/ +/');
+    const args = message.content.slice(prefix.length).split(' ');
     const commandName = args.shift().toLowerCase();
 
     if (!client.commands.has(commandName)) return;
